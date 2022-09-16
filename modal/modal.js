c231705326fb3d664811066e1b8815be4ca243c6
Bryann Henrique <bryann.enrique@gmail.com>
2022-09-16 15:28:23 -0300

    Teste de lib

---
+++ modal/modal.js
@@ -0,0 +1,31 @@
+"strict mode"
+
+export const modal = (() => {
+    function events() {
+        document.addEventListener('click', event => {
+            const { target } = event;
+
+            if (target.classList.contains('overlay')) {
+                const modal = target.closest('.modal');
+                modal.style.display = 'none';
+            }
+            else if (target.classList.contains('modal-trigger')) {
+                const selector = target.dataset.modal;
+                const modal = document.querySelector(selector);
+                
+                modal.style.display = 'block';
+            }
+        });
+    }
+
+
+    function init() {
+        
+        events();
+    }
+
+
+    return {
+        init
+    }
+})();
